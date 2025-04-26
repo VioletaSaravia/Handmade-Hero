e4cdb30cd315c729880fb3e1f8bfbafd7480d7a1
Violeta Saravia <saraviavioleta@gmail.com>
2025-04-26 00:35:14 -0300

    test files

--- src/games/game.c
+++ src/games/game.c
@@ -3,6 +3,7 @@
 struct GameState {
     Camera  cam;
     VAO     test;
+    Sound   testSound;
     Texture font;
     Arena   scene;
     v4      bgColor;
@@ -10,20 +11,28 @@ struct GameState {
 
 export void Setup() {
     E->Settings = (GameSettings){
-        .name       = (cstr)L"Test Game",
-        .version    = "0.2",
-        .resolution = (v2i){640, 360},
-        .scale      = 1,
+        .name    = (cstr)L"Test Game",
+        .version = "0.2",
     };
 }
 
-export void Init() {}
+export void Init() {
+    S->testSound = LoadSound("data\\test.wav", ONESHOT);
+}
 
 export void Update() {
-    // S          = (GameState *)(((u8 *)EngineGetMemory()) + sizeof(EngineCtx));
     S->bgColor = (v4){0.8, 0.2, 0.3, 1.0};
+
+    if (Input()->keys['P'] == JustPressed) PlaySound(S->testSound);
 }
 
 export void Draw() {
     ClearScreen(S->bgColor);
+    for (size_t i = 0; i < 10; i++) {
+        f32 v = 0.1 + 0.1 * i;
+        if (Time() % 1000 > i * 100)
+            DrawRectangle(
+                (Rect){120 + 20 * i /*+ sin(Time() * 0.001) * 100*/, 40 + 20 * i, 200, 100},
+                (v4){v, v, v, v}, 20);
+    }
 }
